10c0dd2a39343735b41afb53a8f3a4e3d2aab7a2
jieun410 <easyeun410@gmail.com>
2024-09-04 08:24:43 +0900

    main_section3

--- src/section2.js
+++ src/section2.js
@@ -11,7 +11,7 @@ return (
         <h2 className="title" color='white'>2024 HCI LAB 연구생 모집</h2>
 
         <div className='textbox_contents'>
-            <p  align="center">모집 단위 및 대상 : 학부생, 석사, 박사과정 및 석박사 통합과정<br/> E-mail : easyeun21@cu.ac.kr <br/> 관심 있는 학생은 위 메일로 연락주시기 바랍니다</p>
+            <p  align="center">모집 단위 및 대상 : 학부생, 석사, 박사과정 및 석박사 통합과정<br/> E-mail : easyeun21@cu.ac.kr <br/> 관심 있는 학생은 위 메일로 연락주시기 바랍니다.</p>
         </div>
     </div>
 
--- src/section3.js
+++ src/section3.js
@@ -10,10 +10,17 @@ return (
     <div className='about'>
         <span>About Us</span>
         <div className='about_intro'>
-            <span><br/>저희 연구실은 사용자 최적의 경험을 위해 UI/UX를 개선하고 연구를 하며, 다양한 측면에서 문제를 찾아 개선할 수 있도록 개발하는 것을 목표로 하고 있습니다.</span>
+            <span>저희 연구실은 사용자 최적의 경험을 위해 UI/UX를 개선하고 연구를 하며, 다양한 측면에서 문제를 찾아 개선할 수 있도록 개발하는 것을 목표로 하고 있습니다.</span>
+        </div>
+
+        <div className='research'>
+            <span>Research Projects</span>
+            <div className='research_intro'>
+                <span> </span>
+                <img></img>
+            </div>
         </div>
     </div>
-
 
 
         
